e2d70ba4294f86e16608ee64a68fa2538f36ef95
Nishant Jadhav <nishantjadhav.cs@gmail.com>
2025-05-22 00:42:37 +0530

    Trusted by section added

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const mobileMenu = document.getElementById('mobileMenu');
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
 });
+
+
+const carousel = document.querySelector('.trusted-items');
+const leftBtn = document.querySelector('.carousel-arrow.left');
+const rightBtn = document.querySelector('.carousel-arrow.right');
+
+leftBtn.addEventListener('click', () => {
+    carousel.scrollBy({ left: -carousel.offsetWidth / 2, behavior: 'smooth' });
+});
+
+rightBtn.addEventListener('click', () => {
+    carousel.scrollBy({ left: carousel.offsetWidth / 2, behavior: 'smooth' });
+});
